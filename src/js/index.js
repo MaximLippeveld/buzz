67296f4d43bf0ecb9103313a03ffa9612d87a766
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-04-12 21:45:00 +0200

    Add brushing and quadtree search, brush toggle with click

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 import 'alpinejs';
 import papa from 'papaparse';
-import { scatter } from './scatter.js'
+import { scatter } from './scatter.js';
+import feather from 'feather-icons';
 
 const app = function() {
     return {
         load: function() {
-
-            const parse = this.parse;
+            feather.replace()
 
             papa.parse("http://127.0.0.1:8000/VIB/Vulcan/vib-vulcan-metadata/representations/umap/Slava_PBMC/data.csv", {
                 download: true,
--- src/js/scatter.js
+++ src/js/scatter.js
@@ -1,6 +1,6 @@
 import * as d3 from 'd3';
 import * as fc from 'd3fc'
-import * as feather from 'feather-icons';
+import feather from 'feather-icons';
 
 export const scatter = function(data) {
 
@@ -9,6 +9,12 @@ export const scatter = function(data) {
     var height = d3.select("body").node().getBoundingClientRect().height;
     canvas.width = width;
     canvas.height = height;
+    
+    const quadtree = d3
+        .quadtree()
+        .x(d => d.dim_1)
+        .y(d => d.dim_2)
+        .addAll(data);
 
     const xScale = d3.scaleLinear()
             .domain([d3.min(data, d => d.dim_1), d3.max(data, d => d.dim_1)])
@@ -30,12 +36,39 @@ export const scatter = function(data) {
                 .requestRedraw();
         })
 
-    const quadtree = d3
-        .quadtree()
-        .x(d => d.dim_1)
-        .y(d => d.dim_2)
-        .addAll(data);
-                
+    var brushDomains = []
+    const brush = fc.brush()
+        .on("brush end", function(event) {
+            brushDomains = [
+                [event.xDomain[0], event.yDomain[0]],
+                [event.xDomain[1], event.yDomain[1]]
+            ]
+        })
+
+    function search([[x0, y0], [x3, y3]]) {
+        quadtree.visit((node, x1, y1, x2, y2) => {
+            if (!node.length) {
+                do {
+                    const d = node.data;
+                    const x = node.data.dim_1;
+                    const y = node.data.dim_2;
+                    d.scanned = true;
+                    d.selected = x >= x0 && x < x3 && y >= y0 && y < y3;
+                } while ((node = node.next));
+            }
+            return x1 >= x3 || y1 >= y3 || x2 < x0 || y2 < y0;
+        });
+    }
+    d3.select("body")
+        .on("keydown.endbrush", function(event) {
+            if (event.keyCode == 13) {
+                search(brushDomains)
+                d3.select('d3fc-group')
+                    .node()
+                    .requestRedraw();
+            }
+        })
+
     function annotation(x, y, data) {
 
         const container = d3
@@ -66,7 +99,7 @@ export const scatter = function(data) {
                     img_container
                         .style("margin-left", leftMargin + 'px')
                 })
-                .html(feather.icons["chevron-left"].toSvg(), {'width': 10, 'height': 15})
+                .html(feather.icons["chevron-left"].toSvg())
             
             const img_container = img_section
                 .append("div")
@@ -118,45 +151,82 @@ export const scatter = function(data) {
     const chart = fc
         .chartCartesian(xScale, yScale)
         .webglPlotArea(
-            fc
-                .seriesWebglMulti()
-                .series([
-                    fc.seriesWebglPoint()
-                        .equals((a, b) => a === b)
-                        .crossValue(d => d.dim_1)
-                        .mainValue(d => d.dim_2)
-                        .type(d3.symbolCircle)
-                        .size(3)
-                        .decorate(program => {
-                            fillColor(program)
-                        })
-                ])
-                .mapping(d => d)
+            fc.seriesWebglPoint()
+                .equals((a, b) => a === b)
+                .crossValue(d => d.dim_1)
+                .mainValue(d => d.dim_2)
+                .type(d3.symbolCircle)
+
+                // TODO: correctly display selection
+                .size(d => {
+                    if (d.selected) {
+                        return 20;
+                    } else if (d.scanned) {
+                        return 5;
+                    } else {
+                        return 1;
+                    }
+                })
+                .decorate(program => {
+                    fillColor(program)
+                })
         )
+        .svgPlotArea(brush)
         .decorate(sel => {
 
+            // the brush SVG is hidden initially so that exploration is possible
+            const brushArea = sel
+                .enter()
+                .select('d3fc-svg.svg-plot-area')
+                .classed("hidden", true)
+
+            // the user can press 'b' to enable brushing
+            // pressing 'b' unhides the brush SVG, disabling exploration
+            function toggleBrush() {
+                    brushArea.classed("hidden", !brushArea.classed("hidden"))
+                    d3.select('d3fc-group')
+                        .node()
+                        .requestRedraw();
+
+                    // update toggle icon
+                    if (brushArea.classed("hidden")) {
+                        d3.select("#controls #brush-toggle .icon").attr("data-feather", "toggle-left")
+                    } else {
+                        d3.select("#controls #brush-toggle .icon").attr("data-feather", "toggle-right")
+                    }
+                    feather.replace()
+            }
+            d3.select("body").on("keydown.enablebrush", event => { if (event.key == "b") toggleBrush() })
+            d3.select("#brush-toggle").on("click", toggleBrush)
+
+            // setup zooming and panning
             sel
                 .enter()
-                .select('d3fc-canvas.plot-area')
+                .select('d3fc-canvas.webgl-plot-area')
                 .on("measure.range", (event, d) => {
                     xScaleOriginal.range([0, event.detail.width]);
                     yScaleOriginal.range([event.detail.height, 0]);
                 })
                 .call(zoom)
 
+            // setup annotations
             sel
-                .select("d3fc-canvas.plot-area")
+                .select("d3fc-canvas.webgl-plot-area")
                 .on("click", (event, d) => {
+
+                    // clicking without holding ctrl, removes all current annotations
                     if (!event.ctrlKey) {
                         d3.select("div#chart").selectAll("div.annotation").remove()
                     }
 
+                    // find closest point on the canvas
                     const coord = d3.pointer(event);
                     const x = xScale.invert(coord[0]);
                     const y = yScale.invert(coord[1]);
                     const radius = Math.abs(x - xScale.invert(coord[0] - 20));
                     const c = quadtree.find(x, y, radius);
 
+                    // if a point is found, draw an annotation
                     if(c != null) {
                         annotation(coord[0], coord[1], c)
                     }
