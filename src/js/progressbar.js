dd605de0321d7f0b87c756166bb8babe91f3323b
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-05-19 16:19:06 +0200

    Add license header

--- src/js/progressbar.js
+++ src/js/progressbar.js
@@ -1,3 +1,20 @@
+// Copyright (C) 2021 Maxim Lippeveld
+// 
+// This file is part of Buzz.
+// 
+// Buzz is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Buzz is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Buzz.  If not, see <http://www.gnu.org/licenses/>.
+
 import * as d3 from 'd3';
 
 export const progress = function({
