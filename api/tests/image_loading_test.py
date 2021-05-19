0ab0a0b5cd16dd6d30826ff511e2639199f2c677
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-05-19 14:37:26 +0200

    Remove old API

--- api/tests/image_loading_test.py
+++
@@ -1,7 +0,0 @@
-from api import main
-
-def test_should_load_multipage_tiff():
-    path = "api/tests/assets/multipage.tiff"
-    data = main.load_image(path)
-    for i, d in enumerate(data):
-        assert d is not None, "Value was None, should be data (channel %d)" % i
