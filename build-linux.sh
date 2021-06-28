94046354f3ab7dca3281619e2e1cfac987583cf2
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-06-28 11:49:47 +0200

    Build script

---
+++ build-linux.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+
+# build tailwind and snowpack
+npm run build
+
+# nwjs
+wget https://dl.nwjs.io/v0.54.0/nwjs-v0.54.0-linux-x64.tar.gz
+tar xzf nwjs-v0.54.0-linux-x64.tar.gz
+mv nwjs-v0.54.0-linux-x64/* buzz/
+mv buzz/nw buzz/buzz
+
+# compress
+VERSION=$(grep version package.json | sed 's/[^:]\+:[^0-9]\+\([^"]\+\).*/\1/')
+mv buzz buzz-$VERSION
+tar -czf buzz-$VERSION.tar.gz buzz-$VERSION/
+
+# clean up
+rm -r buzz-$VERSION nwjs-v*
--- snowpack.config.js
+++ snowpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     /* ... */
   },
   buildOptions: {
-    out: "build",
+    out: "buzz",
     htmlFragments: true
   },
 };
