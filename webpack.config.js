b1de0aa8955947955db213a9bb00e0b3246e2a83
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-06-29 11:03:14 +0200

    Move to webpack serve

--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,56 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: "development",
-    resolve: {
-        alias: {
-            app: path.resolve(__dirname, './src/js/app.js')
-        }
-    },
-    entry: './src/js/index.js',
-    context: __dirname,
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'buzz/package.nw'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(svg|feather)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/[hash][ext][query]'
-                }
-            },
-            {
-                // nw.js package file
-                test: /assets\/package\.json$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'package.json'
-                }
+module.exports = (env) => {
+    return {
+        mode: env.WEBPACK_SERVE ? "development" : "pruduction",
+        resolve: {
+            alias: {
+                app: path.resolve(__dirname, './src/js/app.js')
             }
-        ]
-    }
+        },
+        devtool: env.WEBPACK_SERVE ? "inline-source-map" : "source-map",
+        entry: './src/js/index.js',
+        context: __dirname,
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, 'buzz/package.nw'),
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                filename: 'index.html',
+                inject: 'body'
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].css'
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(svg|feather)$/,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'assets/[hash][ext][query]'
+                    }
+                },
+                {
+                    // nw.js package file
+                    test: /assets\/package\.json$/,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'package.json'
+                    }
+                }
+            ]
+        }
+    };
 };
